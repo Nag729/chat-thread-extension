f3ca78f4f487cd160102dd8e6489d48495f1d20e
長町俊吾 <nagamaaaaachi@gmail.com>
2020-10-06 21:59:22 +0900

    fix start logic

--- src/main.js
+++ src/main.js
@@ -2,21 +2,19 @@ import { FontAwesomeIcon } from '@/plugins/font-awesome';
 import { createApp } from 'vue';
 import App from './App.vue';
 
-// TODO: setTimeoutをもうちょっといい方法に変える
-// redirectの場合に備えて5秒待つ
-setTimeout(() => {
-  // Google Chat が DOM を読み込んでから実行する
-  window.onload = () => {
-    const element = document.querySelector('body > div');
+const extensionStart = () => {
+  // DOM を更新
+  const element = document.querySelector('body > div');
 
-    // メインの div に Vue を読み込ませる用のタグを追加
-    const createdDom = document.createElement('div');
-    createdDom.id = 'thread-extension';
-    element.insertAdjacentElement('beforeend', createdDom);
+  // メインの div に Vue を読み込ませる用のタグを追加
+  const createdDom = document.createElement('div');
+  createdDom.id = 'thread-extension';
+  element.insertAdjacentElement('beforeend', createdDom);
 
-    // Vue インスタンスの生成
-    createApp(App)
-      .component('fa', FontAwesomeIcon)
-      .mount('#thread-extension');
-  };
-}, 5000);
+  // Vue インスタンスの生成
+  createApp(App)
+    .component('fa', FontAwesomeIcon)
+    .mount('#thread-extension');
+};
+
+extensionStart();
